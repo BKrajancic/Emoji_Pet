cd7355e4411939b0cbe34431e1d85361e8ee6570
Benjamin Krajancic <benjamin.krajancic@student.uts.edu.au>
2019-07-28 09:56:53 +1000

    fixing interaction handler

--- arduino_interaction_handler.py
+++ arduino_interaction_handler.py
@@ -12,47 +12,39 @@ class Interaction_Handler():
         self.button_r = 2
         grovepi.pinMode(self.button_l, "INPUT")
         grovepi.pinMode(self.button_r, "INPUT")
-        self.rac_1 = 3
-        self.rac_2 = 4
         self.rac_max = 300
         self.grove_vcc = 5
         self.adc_ref = 5
+
+        self.rac_1 = 3
+        self.rac_1_prev = self.get_rotation(grovepi.analogRead(self.rac_1))
         grovepi.pinMode(self.rac_1, "INPUT")
 
-        self.rac_1_prev = self.get_rotation(grovepi.analogRead(self.rac_1))
+        self.rac_2 = 4
         self.rac_2_prev = self.get_rotation(grovepi.analogRead(self.rac_2))
+        grovepi.pinMode(self.rac_2, "INPUT")
 
     def get(self) -> List[Tuple[INTERACTION, int]]:
         events = pygame.event.get()
         interactions: List[Tuple[INTERACTION, int]] = []
-        for event in events:
-            if event.type == pygame.KEYDOWN:
-                if grovepi.digitalRead(self.button_l) == 1:
-                    interactions.append((INTERACTION.BUTTON_L, 0))
-                if grovepi.digitalRead(self.button_r) == 1:
-                    interactions.append((INTERACTION.BUTTON_R, 0))
 
-                rac_1_rot = self.get_rotation(grovepi.analogRead(self.rac_1))
-                rac_2_rot = self.get_rotation(grovepi.analogRead(self.rac_1))
+        if grovepi.digitalRead(self.button_l) == 1:
+            interactions.append((INTERACTION.BUTTON_L, 0))
+        if grovepi.digitalRead(self.button_r) == 1:
+            interactions.append((INTERACTION.BUTTON_R, 0))
 
-                if (rac_1_rot != self.rac_1_prev):
-                    pass
-                if (rac_2_rot != self.rac_2_prev):
-                    pass
+        rac_1_rot = self.get_rotation(grovepi.analogRead(self.rac_1))
+        if (rac_1_rot != self.rac_1_prev):
+            p = (INTERACTION.ROLL, int(abs(self.rac_1_prev - rac_1_rot)))
+            interactions.append(p)
+        self.rac_1_prev = rac_1_rot
 
-                self.rac_1_prev = rac_1_rot
-                self.rac_2_prev = rac_2_rot
+        rac_2_rot = self.get_rotation(grovepi.analogRead(self.rac_2))
+        if (rac_2_rot != self.rac_2_prev):
+            p = (INTERACTION.ROLL, int(abs(self.rac_2_prev - rac_2_rot)))
+            interactions.append(p)
+        self.rac_2_prev = rac_2_rot
 
-                if grovepi.analogRead(self.rac_1) == pygame.K_UP:
-                    interactions.append((INTERACTION.ROLL, -8))
-                if event.key == pygame.K_DOWN:
-                    interactions.append((INTERACTION.ROLL, -8))
-                if event.key == pygame.K_COMMA:
-                    interactions.append((INTERACTION.BUTTON_LESS_THAN, 0))
-                if event.key == pygame.K_PERIOD:
-                    interactions.append((INTERACTION.BUTTON_GREATER_THAN, 0))
-            elif event.type == QUIT:
-                interactions.append((INTERACTION.EXIT, 0))
         return interactions
 
     def get_rotation(self, rac_read):
