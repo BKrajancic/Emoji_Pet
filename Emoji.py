3095f08be82294f33b77aedc55bc63e64c69a640
Benjamin Krajancic <benjamin.krajancic@student.uts.edu.au>
2019-07-27 13:48:03 +1000

    added submodule for sprites

--- Emoji.py
+++ Emoji.py
@@ -8,10 +8,10 @@ class Emoji(Entity):
         Entity.__init__(self, x, y)
         # https://openmoji.org/python3 -m pip install -U pygame --user
         self.files = {
-            "happy": pygame.image.load("sprites/1F60A.png").convert_alpha(),
-            "moody": pygame.image.load("sprites/1F62b.png").convert_alpha(),
-            "tongue": pygame.image.load("sprites/1F60B.png").convert_alpha(),
-            "sad": pygame.image.load("sprites/1F61F.png").convert_alpha()
+            "happy": pygame.image.load("openmoji/color/72x72/1F60A.png").convert_alpha(),
+            "moody": pygame.image.load("openmoji/color/72x72/1F62b.png").convert_alpha(),
+            "tongue": pygame.image.load("openmoji/color/72x72/1F60B.png").convert_alpha(),
+            "sad": pygame.image.load("openmoji/color/72x72/1F61F.png").convert_alpha()
         }
 
         self.set_motion(vel_x=3, accel_x=-0.3, vel_y=3, accel_y=-1)
--- Hat.py
+++ Hat.py
@@ -5,7 +5,8 @@ from Entity import *
 class Hat(Entity):
     def __init__(self, x: int, y: int):
         Entity.__init__(self, x, y)
-        self.sprite = pygame.image.load("sprites/1F62b.png").convert_alpha()
+        self.sprite = pygame.image.load(
+            "openmoji/color/72x72/1F62b.png").convert_alpha()
         self.set_motion(vel_x=1, accel_x=0.01, vel_y=1, accel_y=-1)
 
     def draw(self, screen):
